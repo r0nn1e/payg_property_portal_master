eae48c7ec8d04e51e937ee6d84ffc058346554e0
Ron.McCormack <rmcweb@outlook.com>
2015-07-01 12:32:47 +0100

    Added Menu structure only

---
+++ payg_property_portal_master_scratch/payg_demo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(payg_demo.Startup))]
+namespace payg_demo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
